b3b90f3183a9e830c17581d945cd90baea3c29d8
rojinatiwari <rojinatiwari17@gmail.com>
2024-08-08 11:04:19 +0545

    creating and destroying php session

---
+++ session.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<?php
+session_start();
+if(isset($_SESSION['counter']))
+{
+    $_SESSION['counter'] += 1;
+}else{
+    $_SESSION['counter'] = 1;
+}
+$msg = "You have visited this page ".$_SESSION['counter']. " in this session";
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Setting up a PHP session </title>
+</head>
+<body>
+    <?php
+    echo($msg);
+    session_destroy();
+    ?>
+    
+</body>
+</html>
