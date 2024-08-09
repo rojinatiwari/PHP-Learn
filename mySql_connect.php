8a01b629d11d9c425b222a154705dfc91936f03c
rojinatiwari <rojinatiwari17@gmail.com>
2024-08-10 05:23:26 +0545

    creating database

--- mySql_connect.php
+++ mySql_connect.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "local host";
+$servername = "localhost";
 $username = "username";
 $password = "";
 
@@ -12,4 +12,14 @@ if(!$conn)
     die("connection failed :".mysqli_connect_error());
 }
 echo" connected successfully";
+
+//create database
+$sql = "CREATE DATABASE db_student";
+if(mysqli_query($conn,$sql))
+{
+    echo"database created sucessfully";
+}else{
+    echo"error creating database".mysqli_error($conn);
+}
+mysqli_close($conn);
 ?>
