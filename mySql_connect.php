037e264ec150b2d9a216cbf795cf14a75ad7be05
rojinatiwari <rojinatiwari17@gmail.com>
2024-08-11 10:05:06 +0545

    debugged code

--- mySql_connect.php
+++ mySql_connect.php
@@ -1,37 +1,46 @@
 <?php
+
+
+// Create connection
 $servername = "localhost";
-$username = "username";
-$password = "";
+$username = "root"; // Default MySQL username is usually 'root'
+$password = ""; // Default password is usually empty for 'root' on a local server
+$db_name = "db_student";
 
-// create connection
-$conn = mysqli_connect($servername,$username,$password);
+$conn = mysqli_connect($servername, $username, $password);
 
-//check connection
-if(!$conn)
-{
-    die("connection failed :".mysqli_connect_error());
+
+
+
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-echo" connected successfully";
+echo "Connected successfully<br>";
 
-//create database
+// Create database
 $sql = "CREATE DATABASE db_student";
-if(mysqli_query($conn,$sql))
-{
-    echo"database created sucessfully";
-}else{
-    echo"error creating database".mysqli_error($conn);
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully<br>";
+} else {
+    echo "Error creating database: " . mysqli_error($conn) . "<br>";
 }
 
-//creating table 
-$sql = "CREATE TABLE tbl_student(id INT (6)UNSIGNED AUTO_INCREMENT PRIMARY KEY , firstName VARCHAR(30)NOT NULL,lastName VARCHAR(30)NOT NULL)";
-if(mysqli_query($conn,$sql))
-{
-    echo"Table tbl_student created successfully";
-}else{
-    echo"Error creating table : ".mysqli_error($conn);
+// Select the database
+mysqli_select_db($conn, "db_student");
+
+// Create table
+$sql = "CREATE TABLE tbl_student (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL
+)";
+if (mysqli_query($conn, $sql)) {
+    echo "Table tbl_student created successfully<br>";
+} else {
+    echo "Error creating table: " . mysqli_error($conn) . "<br>";
 }
 
 mysqli_close($conn);
 ?>
-
-
