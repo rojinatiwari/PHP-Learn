f73b412d560da4f75a3748675a39c8ce826692c0
rojinatiwari <rojinatiwari17@gmail.com>
2024-08-14 00:16:25 +0545

    draft

--- mySql_connect.php
+++ mySql_connect.php
@@ -59,7 +59,8 @@ if(mysqli_num_rows($result)>0){
     //output data of each row
     while($row = mysqli_fetch_assoc($result)){
         echo"id;".$row["id"]."Name:".$row["firstname"].$row["lastname"]."<br>";
- }else
+    }
+ else
     {
     echo"0 results";
 }
