4584d6dbbd89f611520eee27c11fc7b910d2fa71
rojinatiwari <rojinatiwari17@gmail.com>
2024-08-20 20:04:24 +0545

    draft

--- mySql_database.php
+++ mySql_database.php
@@ -9,10 +9,6 @@ $db_name = "db_student";
 
 $conn = mysqli_connect($servername, $username, $password);
 
-
-
-
-
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
@@ -44,54 +40,47 @@ if (mysqli_query($conn, $sql)) {
 //insert data
 $sql = "INSERT INTO tbl_student(firstname,lastname,email)VALUES('Rojina','Tiwari','rojina@gmail.com','Pokhara')";
 
-if(mysqli_query($conn,$sql)){
-echo "New record created successfully";
-}
-else
-{
-echo "Error :".$sql."<br>" . mysqli_error($conn);
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error :" . $sql . "<br>" . mysqli_error($conn);
 }
 
 //select data
 $sql = "SELECT id,firstname ,lastname FROM tbl_student";
-$result = mysqli_query($conn,$sql);
-if(mysqli_num_rows($result)>0){
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
     //output data of each row
-    while($row = mysqli_fetch_assoc($result)){
-        echo"id;".$row["id"]."Name:".$row["firstname"].$row["lastname"]."<br>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "id;" . $row["id"] . "Name:" . $row["firstname"] . $row["lastname"] . "<br>";
     }
-
 }
 //update database
 $sql = "UPDATE tbl_student SET lastname ='Ram' WHERE id = 2";
-if(mysqli_query($conn,$sql)){
-    echo"Record updated successfully!";
-}else{
-    "Error updating record :".mysqli_erroe($conn);
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully!";
+} else {
+    "Error updating record :" . mysqli_error($conn);
 }
 
+
+
 //delete database
-$sql="DELETE FROM tbl_student WHERE id=3 ";
-if($mysqli_query($conn,$sql))
-{
-    echo"Record deeted successfully";
-
-}else{
-    echo"Error deleting record:".mysqli_error($conn);
-
+$sql = "DELETE FROM tbl_student WHERE id=3 ";
+if ($mysqli_query($conn, $sql)) {
+    echo "Record deeted successfully";
+} else {
+    echo "Error deleting record:" . mysqli_error($conn);
 }
 //insert multiple data
 
-$sql="INSERT INTO tbl_student (firstName,lastName,email,address)VALUES('ROJINA','TIWARI','POKHARA)";
-$sql="INSERT INTO tbl_student (firstName,lastName,email,address)VALUES('ROSHAN',TIWARI','POKHARA')";
-$sql="INSERT INTO tbl_student (firstName,lastName,email,address)VALUES('ROJAN','TIWARI','POKHARA')";
+$sql = "INSERT INTO tbl_student (firstName,lastName,email,address)VALUES('ROJINA','TIWARI','POKHARA)";
+$sql = "INSERT INTO tbl_student (firstName,lastName,email,address)VALUES('ROSHAN',TIWARI','POKHARA')";
+$sql = "INSERT INTO tbl_student (firstName,lastName,email,address)VALUES('ROJAN','TIWARI','POKHARA')";
 
-if (mysqli_query($conn,$sql))
-{
-    echo"Inserted multiple data successfully";
-}else{
-    echo"error created during insertinr multiple data".mysqli_error($conn);
+if (mysqli_query($conn, $sql)) {
+    echo "Inserted multiple data successfully";
+} else {
+    echo "error created during insertinr multiple data" . mysqli_error($conn);
 }
 mysqli_close($conn);
-?>
-
