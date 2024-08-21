40301e97df6b6083ec8d655a3576e52534c1c262
rojinatiwari <rojinatiwari17@gmail.com>
2024-08-21 10:01:08 +0545

    setting and destroying session in php

--- session.php
+++ session.php
@@ -1,27 +1,28 @@
-
 <!DOCTYPE html>
 <?php
 session_start();
-if(isset($_SESSION['counter']))
-{
+if (isset($_SESSION['counter'])) {
     $_SESSION['counter'] += 1;
-}else{
+} else {
     $_SESSION['counter'] = 1;
 }
-$msg = "You have visited this page ".$_SESSION['counter']. " in this session";
+$msg = "You have visited this page " . $_SESSION['counter'] . " in this session";
 
 ?>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Setting up a PHP session </title>
 </head>
+
 <body>
     <?php
-    echo($msg);
+    echo ($msg);
     session_destroy();
     ?>
-    
+
 </body>
+
 </html>
