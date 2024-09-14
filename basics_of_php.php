b7f47f548741f784735e85e9db3340ec5a866c16
rojinatiwari <rojinatiwari17@gmail.com>
2024-09-14 19:05:08 +0545

    basics of php

--- basics_of_php.php
+++ basics_of_php.php
@@ -107,7 +107,7 @@ function myTest() {
 //   //php echo statement
 echo "<h2>PHP is Fun!</h2>";  
 echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
+echo "I am about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with  multiple parameters.";
 
 //php print statement
